62d3212546c8b5e53333adcae912130fdb6d175c
Ahmad Raza <ahmadrazajutt.7761@gmail.com>
2025-01-29 00:55:56 +0500

    Assignment 10

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment#10
+// Dear Students.
+// Please follow the instructions https://docs.google.com/document/d/1zxsdARmEOYr_8djErVv-KoklS3WasOcnQjEc19WhGsk/edit?usp=sharing
+
+// Submit: https://forms.gle/JT1iMYfB3dExtT4M6
+// Deadline: 26 Jan, 2025
+
+
+
+// 1. Write a js program to find length of a string.
+// var str="Hello World";
+// console.log(str.length)
+
+// 2. Write a js program to copy one string to another string.
+// var scr_String="Hello World";
+// var secont_String=scr_String;
+// console.log(scr_String)
+
+const document=document
+console.log(document)
